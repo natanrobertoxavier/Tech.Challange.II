ff0b6c0f26f5eaecb712364edbc8542535d7f10e
Natan Roberto Xavier <natanxavier@policiamilitar.sp.gov.br>
2024-10-07 15:15:56 -0300

    update

--- Tech.Challenge.I/Tech.Challenge.I.Infrastructure/Migrations/Versions/Version004.cs
+++ Tech.Challenge.I/Tech.Challenge.I.Infrastructure/Migrations/Versions/Version004.cs
@@ -20,16 +20,16 @@ public class Version004 : Migration
         Execute.Sql(@"
                     START TRANSACTION;
 
-                    UPDATE TechChallenge.dddregions
+                    UPDATE TechChallenge.DDDRegions
                     SET userId = (
                         SELECT Id
-                        FROM TechChallenge.users
+                        FROM TechChallenge.Users
                         ORDER BY RegistrationDate
                         LIMIT 1
                     )
                     WHERE EXISTS (
                         SELECT 1
-                        FROM TechChallenge.users
+                        FROM TechChallenge.Users
                     );
                     
                     COMMIT;
